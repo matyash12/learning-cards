d59f81c28b27a80cbe5ac06f8b19a04a50e88b80
matyash12 <93146910+matyash12@users.noreply.github.com>
2023-11-18 18:52:30 +0100

    Fast card adding

--- frontend/src/views/CreateNewCard.vue
+++ frontend/src/views/CreateNewCard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { API_ADDRESS } from '../helpers.js';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
@@ -10,10 +10,11 @@ let cardid = route.params.cardid; //new == create new
 
 let visiblePart = ref('');
 let hiddenPart = ref('');
-
+let visiblePartInput = ref(null);
+let wasLastCardCreated = ref(null);
 
 const createCard = () => {
-    axios.post(API_ADDRESS+'card/new', {
+    axios.post(API_ADDRESS + 'card/new', {
         hiddenPart: hiddenPart.value,
         visiblePart: visiblePart.value,
         deckid: deckid
@@ -27,16 +28,53 @@ const createCard = () => {
     )
         .then(function (response) {
             console.log(response);
-            moveToDeckView();
+            visiblePart.value = '';
+            hiddenPart.value = '';
+            visiblePartInput.value.focus();
+            wasLastCardCreated.value = true;
+            router.push('/deck/'+deckid+'/new')
+            //moveToDeckView();
         })
         .catch(function (error) {
             console.log(error);
+            wasLastCardCreated.value = false;
         });
 
 };
-const moveToDeckView = () =>{
-    router.push('/deck/'+deckid)
+const moveToDeckView = () => {
+    router.push('/deck/' + deckid)
 }
+
+const handleKeyDown = (event) => {
+    if (event.key === 'Tab' && event.target.id === 'visiblePart') {
+        createCard();
+    }
+    
+    const isInputField = event.target.tagName.toLowerCase() === 'input' ||
+        event.target.tagName.toLowerCase() === 'textarea';
+
+    
+    // If it's an input field, do nothing
+    if (isInputField) {
+        return;
+    }
+
+
+    if (event.key === 'c') {
+        createCard();
+    }
+    
+    
+};
+
+onMounted(() => {
+    visiblePartInput.value.focus();
+    window.addEventListener('keydown', handleKeyDown);
+});
+onUnmounted(() => {
+    window.removeEventListener('keydown', handleKeyDown);
+});
+
 </script>
 
 
@@ -49,23 +87,24 @@ const moveToDeckView = () =>{
         <div class="field">
             <label class="label">Hidden side</label>
             <div class="control">
-                <input v-model="hiddenPart" class="input" type="text" placeholder="">
+                <input ref="visiblePartInput"  v-model="hiddenPart" class="input" type="text" placeholder="">
             </div>
         </div>
         <div class="field">
             <label class="label">Visible side</label>
             <div class="control">
-                <input v-model="visiblePart" class="input" type="text" placeholder="">
+                <input id="visiblePart" v-model="visiblePart" class="input" type="text" placeholder="">
             </div>
         </div>
 
 
-
+        <p v-if="wasLastCardCreated == true">Card was created</p>
 
         <div class="field is-grouped">
             <div class="control">
+                <button @click="createCard" class="button">Create (c)</button>
                 <button @click="moveToDeckView" class="button">Cancel</button>
-                <button @click="createCard" class="button">Create</button>
+                
             </div>
         </div>
     </div>
