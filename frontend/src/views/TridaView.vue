21a18db21aa2f2aad54b5739a2b01d4a4fdc7839
matyash12 <93146910+matyash12@users.noreply.github.com>
2023-12-04 22:42:54 +0100

    Burger menu fix on mobile

--- frontend/src/views/TridaView.vue
+++ frontend/src/views/TridaView.vue
@@ -129,73 +129,44 @@ const hideDeleteConfirmationModal = () => {
     isDeleteConfirmationModalActive.value = false;
 };
 
+const isBurgerMenuOpen = ref(false);
+
+const toggleBurgerMenu = () => {
+  isBurgerMenuOpen.value = !isBurgerMenuOpen.value;
+};
+
 </script>
 
 
 <template>
     <!--Navbar-->
     <header>
-        <div class="wrapper">
-            <nav class="navbar" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    <!-- <a class="navbar-item" href="https://bulma.io">
-                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-                    </a> -->
-
-                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-                        data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="navbarBasicExample" class="navbar-menu">
-                    <!-- <div class="navbar-start">
-                        <a class="navbar-item">
-                            Home
-                        </a>
-
-                        <a class="navbar-item">
-                            Documentation
-                        </a>
-
-                        <div class="navbar-item has-dropdown is-hoverable">
-                            <a class="navbar-link">
-                                More
-                            </a>
-
-                            <div class="navbar-dropdown">
-                                <a class="navbar-item">
-                                    About
-                                </a>
-                                <a class="navbar-item">
-                                    Jobs
-                                </a>
-                                <a class="navbar-item">
-                                    Contact
-                                </a>
-                                <hr class="navbar-divider">
-                                <a class="navbar-item">
-                                    Report an issue
-                                </a>
-                            </div>
-                        </div>
-                    </div> -->
-
-                    <div class="navbar-end">
-                        <div class="navbar-item">
-                            <div class="buttons">
-                                <a class="button is-light" @click="logOut">
-                                    Log out
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+    <div class="wrapper">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a role="button" class="navbar-burger" aria-label="menu"
+             :class="{ 'is-active': isBurgerMenuOpen }"
+             @click="toggleBurgerMenu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
-    </header>
+
+        <div id="navbarBasicExample" :class="{ 'is-active': isBurgerMenuOpen }" class="navbar-menu">
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-light" @click="logOut">
+                  Log out
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
 
 
     <div class="columns">
