4a2a136ae016bc7b9706f837e6d0b782b4b00c51
matyash12 <93146910+matyash12@users.noreply.github.com>
2023-12-17 12:59:35 +0100

    Revert "pwa"
    This reverts commit a6ba7e5e9f0e6fe76788b6f3a461cf454bb0fbca.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,6 @@ import axios from 'axios';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
 
 const app = createApp(App)
 
