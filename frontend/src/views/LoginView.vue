a247e455df38796bfa8eb12a3df23eece41d8cfb
matyash12 <93146910+matyash12@users.noreply.github.com>
2023-12-17 11:30:01 +0100

    PWA

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,41 +1,44 @@
 <template>
-    <div>
-      <h1 class="title">Login</h1>
-  
-      <form @submit.prevent @keyup.enter="loginRequest">
-        <div class="box">
-          <div class="field">
-            <label class="label">Email</label>
-            <div class="control">
-              <input v-model="email" class="input" type="email" placeholder="Email" autocomplete="email">
+  <div class="container">
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <h1 class="title">Login</h1>
+
+        <form @submit.prevent @keyup.enter="loginRequest">
+          <div class="box">
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
+                <input v-model="email" class="input" type="email" placeholder="Email" autocomplete="email">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input v-model="password" class="input" type="password" placeholder="Password" autocomplete="current-password">
+              </div>
+            </div>
+            <div class="notification is-danger" v-show="showWarning">
+              <button class="delete" @click="closeWarning"></button>
+              <p v-text="warningMessage"></p>
+            </div>
+            <div class="field is-grouped">
+              <div class="control">
+                <button class="button is-primary" @click="loginRequest">Login</button>
+              </div>
+              <div class="control">
+                <button class="button is-link is-light" @click="recoverPassword">Recover password</button>
+              </div>
+              <div class="control">
+                <button class="button is-link is-light" @click="createAccount">Create account</button>
+              </div>
             </div>
           </div>
-          <div class="field">
-            <label class="label">Password</label>
-            <div class="control">
-              <input v-model="password" class="input" type="password" placeholder="Password" autocomplete="current-password">
-            </div>
-          </div>
-          <div class="notification is-danger" v-show="showWarning">
-            <button class="delete" @click="closeWarning"></button>
-            <p v-text="warningMessage"></p>
-          </div>
-          <div class="field is-grouped">
-            <div class="control">
-              <button class="button is-primary" @click="loginRequest">Login</button>
-            </div>
-            <div class="control">
-              <button class="button is-link is-light" @click="recoverPassword">Recover password</button>
-            </div>
-            <div class="control">
-              <button class="button is-link is-light" @click="createAccount">Create account</button>
-            </div>
-          </div>
-        </div>
-      </form>
-  
+        </form>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
   
 
 <script setup>
@@ -52,38 +55,39 @@ const password = ref('');
 const showWarning = ref(false);
 const warningMessage = ref('');
 
-const closeWarning = () =>{
-    showWarning.value = false;
-    warningMessage.value = '';
+const closeWarning = () => {
+  showWarning.value = false;
+  warningMessage.value = '';
 }
 
 const loginRequest = () => {
-    axios.post(API_ADDRESS + 'user/login',
-        {
-            "email": email.value,
-            "password": password.value
-        },
-        {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        }
-    ).then(function (result) {
+  axios.post(API_ADDRESS + 'user/login',
+    {
+      "email": email.value,
+      "password": password.value
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }
+  ).then(function (result) {
 
-        router.push('/');
-    }).catch(function (err) {
-        warningMessage.value = err.response.data.message;
-        showWarning.value = true;
-        console.log(err);
-    })
+    router.push('/');
+  }).catch(function (err) {
+    warningMessage.value = err.response.data.message;
+    showWarning.value = true;
+    console.log(err);
+  })
 }
 
 const createAccount = () => {
-    router.push("/user/register");
+  router.push("/user/register");
 }
 
-const recoverPassword = () =>{
+const recoverPassword = () => {
   router.push("/requestnewpassword");
 }
 
 </script>
+
