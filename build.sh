29656d10c3b8baa7e48a549ecc43efea80ad291c
Matyas Hajek <hajekmatyas1@gmail.com>
2023-12-27 14:29:28 +0100

    settings changes

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 ##Define the source and destination file paths
 #Easy startup
-example_file="backend/src/main/resources/application_example.properties"
-destination_file="backend/src/main/resources/application.properties"
+example_file_application_properties="backend/src/main/resources/application_example.properties"
+destination_file_application_properties="backend/src/main/resources/application.properties"
+
+example_file_env=".env.example"
+destination_file_env=".env"
 
 # Check if the "-example-run" argument is provided
-if [[ "$1" == "-example-run" ]]; then
+if [[ "$1" == "-example" ]]; then
   # Copy the example file to the destination
-  cp "$example_file" "$destination_file"
-  echo "Example file copied successfully."
+  cp "$example_file_application_properties" "$destination_file_application_properties"
+   cp "$example_file_env" "$destination_file_env"
+  echo "Example files copied successfully."
 else
   echo "For running as example run it using: $0 -example-run"
 fi
